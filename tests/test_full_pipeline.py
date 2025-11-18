cfae4c0cad1df8ddb9bb9d66e3569cccb9584544
Ian Goodall-Halliwell <90027009+Ian-Goodall-Halliwell@users.noreply.github.com>
2025-11-18 17:04:47 -0500

    Reduce thread count for CI to improve stability

--- tests/test_full_pipeline.py
+++ tests/test_full_pipeline.py
@@ -130,9 +130,9 @@ def test_full_pipeline(quick=False):
         "--qc-csv",
         qc_csv,
         "--synthseg-threads",
-        "8",  # Lower thread count for CI
+        "1",  # Lower thread count for CI
         "--ants-threads",
-        "8",  # Lower thread count for CI
+        "1",  # Lower thread count for CI
         "--compose",
         "--verbose"
     ]
